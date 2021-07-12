ee464fda50163bd56a7afed9956d10a36841bea1
netzen86 <netzen0su@gmail.com>
2021-07-12 23:46:19 +0500

    add func device_console_connect

--- ztceve.py
+++ ztceve.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import csv
+import time
 from netmiko import ConnectHandler
+from netmiko.ssh_dispatcher import redispatch
 
 def get_devices_from_file(device_file):
     # This function takes a CSV file with inventory and creates a python list of dictionaries out of it
@@ -45,7 +47,57 @@ def load_config(conf_name):
     
     return commands
 
+
+def device_console_connect(device, config):
+    repeater = False
+    try:
+        with ConnectHandler(
+            host=device["ip"],
+            port=device["port"],
+            device_type=device["device_type"]) as console:
+            while not repeater:
+                console.write_channel('\r\n')
+                output = console.read_until_pattern("yes|>")
+                print("Found yes/no")
+                if "yes" in output:
+                    console.write_channel('no\n')
+                    #time.sleep(1)
+                console.write_channel('\r\n')
+                #time.sleep(0.5)
+                #console.write_channel('\r\n')
+                console.read_until_pattern(">")
+                #print("Loading finished")
+                #console.write_channel('\r\n')
+                #time.sleep(1)
+                console.write_channel('show clock\n')
+                output = console.read_until_pattern(">")
+                print(output)
+                redispatch(console, device_type='cisco_ios_telnet')
+                output = console.send_command("show clock")
+                print("REDISPATCH SUCCESS", output)
+                console.enable()
+                console.send_config_set(config)
+                repeater = True
+        return output
+    except:
+        print("error")
+
+
+
+
 def device_connect(device, config):
+    with ConnectHandler(
+        host=device["ip"],
+        port=device["port"],
+        username=device["username"],
+        password=device["password"],
+        device_type=device["device_type"],
+        secret=device["secret"]) as net_connect:
+        net_connect.enable()
+        output = net_connect.send_config_set(config)
+    return output
+
+""" def device_connect(device, config):
     #print(config)
     net_connect = ConnectHandler(
         host=device["ip"],
@@ -54,18 +106,14 @@ def device_connect(device, config):
         password=device["password"],
         device_type=device["device_type"],
         secret=device["secret"])
-    net_connect.find_prompt(delay_factor=20)
-    #net_connect.send_command('\n', expect_string=r'.*>')
     net_connect.enable("enable")
-    net_connect.config_mode("configure terminal")
     output = net_connect.send_config_set(config)
-    net_connect.disconnect()
     return output
-
+ """
 
 for device in get_devices_from_file("devices.csv"):
-    #print(f"Device {device}")
+    print(f"Device {device}")
     configuration = load_config(device["conf_name"])
     print(f"Configuration {configuration}")
-    print(device_connect(device, configuration))
+    print(device_console_connect(device, configuration))
    
